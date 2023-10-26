af3838918d2eec593a49f2e637016dbae2989385
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-25 21:35:45 -0700

    adding the test mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,26 +2,30 @@ const request = require('request');
 
 const breedName = process.argv[2];
 
-if (!breedName) {
-  console.log('Please provide a breed name as a command-line argument.');
-  process.exit(1);
-}
-
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+const fetchBreedDescription = function(breedName, callback) {
+  if (!breedName) {
+    callback('please provide a breed name as a command-line argument.', null);
     return;
   }
 
-  const data = JSON.parse(body);
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  if (data.length === 0) {
-    console.log(`Breed '${breedName}' not found.`);
-    return;
-  }
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-  const breedInfo = data[0];
-  console.log(breedInfo.description);
-});
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback(`Breed '${breedName}' not found.`, null);
+      return;
+    }
+
+    const breedInfo = data[0];
+    callback(null, breedInfo.description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
